351ee787ac13df705f9a981972e80dd8cf7d3220
Kirby Campbell <jkirbycampbell@gmail.com>
2018-09-20 17:50:32 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -9,4 +9,12 @@ class Appointment < ActiveRecord::Base
   def doctor_name
     self.doctor ? self.doctor.name : nil
   end
+
+  def patient_name=(name)
+    self.patient = Patient.find_or_create_by(name:name)
+  end
+
+  def patient_name
+    self.patient ? self.patient.name : nil
+  end
 end
