3f1d5c45ccb66b34db5bc7c2243f4acb736310a2
Kirby Campbell <jkirbycampbell@gmail.com>
2018-09-20 17:32:04 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180920172842) do
+ActiveRecord::Schema.define(version: 20180920173052) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "appointment_datetime"
+    t.integer  "patient"
+    t.integer  "doctor"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
